3915ab38132d9daf90ee0c9816e1f453c5749751
brherron <brherron@wisc.edu>
2022-08-12 23:28:22 -0500

    Starting UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,28 @@
 import type { NextPage } from "next";
 import { trpc } from "../utils/trpc";
 
+const QuestionCreator: React.FC = () => {
+  const { mutate } = trpc.useMutation("questions.create")
+
+  return (
+    <input onSubmit={(event) => console.log(event)} />
+  )
+}
+
 const Home: NextPage = (props: any) => {
   const { data, isLoading } = trpc.useQuery(["questions.getAll"]);
 
   if (isLoading || !data) return <div>Loading...</div>
 
-  console.log(data)
-
-  return <div>{data[0]?.question}</div>
+  return (
+    <>
+      <div className="flex flex-col">
+        <div className="text-2xl font-bold">Questions</div>
+        {data[0]?.question}
+      </div>
+      <QuestionCreator />
+    </>
+  )
 };
 
 export default Home;
--- src/server/router/questions.ts
+++ src/server/router/questions.ts
@@ -7,5 +7,16 @@ export const questionRouter = createRouter()
   .query("getAll", {
     async resolve() {
       return await prisma.pollQuestion.findMany();
-    }
+    },
+  }).mutation("create", {
+    input: z.object({
+      question: z.string().min(5).max(600),
+    }),
+    async resolve({input}) {
+      return await prisma.pollQuestion.create({
+        data: {
+          question: input.question,
+        },
+      });
+    },
   });
