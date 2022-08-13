dd4f7becb940db4951a094a81659da32953e2bca
brherron <brherron@wisc.edu>
2022-08-12 23:39:32 -0500

    Ability to add questions.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
 import type { NextPage } from "next";
+import React from "react";
 import { trpc } from "../utils/trpc";
 
 const QuestionCreator: React.FC = () => {
-  const { mutate } = trpc.useMutation("questions.create")
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const client = trpc.useContext();
+  const { mutate, isLoading } = trpc.useMutation("questions.create", {
+    onSuccess: (data) => {
+      client.invalidateQueries('questions.getAll');
+    }
+  })
 
   return (
-    <input onSubmit={(event) => console.log(event)} />
+    <input 
+      ref={inputRef} 
+      disabled={isLoading} 
+      onKeyDown={(event) => {
+        if (event.key === "Enter") {
+          mutate({question: event.currentTarget.value});
+          if (!inputRef.current) return;
+          inputRef.current.value = "";
+        }
+      }} 
+    />
   )
 }
 
@@ -15,13 +32,19 @@ const Home: NextPage = (props: any) => {
   if (isLoading || !data) return <div>Loading...</div>
 
   return (
-    <>
+    <div className="p-6 flex flex-col">
       <div className="flex flex-col">
         <div className="text-2xl font-bold">Questions</div>
-        {data[0]?.question}
+        {data.map(question => {
+          return (
+            <div key={question.id} className="my-2">
+              {question.question}
+            </div>
+          );
+        })}
       </div>
       <QuestionCreator />
-    </>
+    </div>
   )
 };
 
